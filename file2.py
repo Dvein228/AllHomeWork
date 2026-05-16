beb453f08a51ddc041725c325e671acbde1c8b7b
Home <davidfilippov122011@gmail.com>
2026-05-16 17:05:14 +0300

    Несколько классов

---
+++ file2.py
@@ -0,0 +1,13 @@
+class Enemy:
+    def __init__(self, name, health,damage):
+        self.name=name
+        self.health=health
+        self.damage =damage
+
+
+class Character:
+    def __init__(self, name, health,damage):
+        self.name=name
+        self.health=health
+        self.damage= damage
+
