c7f80b08470b41147069bcd7e053da69df08dd0b
Home <davidfilippov122011@gmail.com>
2026-05-22 13:30:22 +0300

    Несколько классов

--- file2.py
+++ file2.py
@@ -1,11 +1,16 @@
+class Item:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
 class Enemy:
     def __init__(self, name, health,damage):
         self.name=name
         self.health=health
         self.damage =damage
-    def attack(self, enemy):
-            enemy.hp -= self.damage
-            print(f"{self.name} атакує {enemy.name} на {self.damage} шкоди!")
+    def attack(self, player):
+            player.health -= self.damage
+            print(f"{self.name} атакує {player.name} на {self.damage} шкоди!")
 
 
 class Character:
@@ -24,5 +29,21 @@ class Character:
                 for item in self.inventory:
                     print(f"- {item.name} (цінність: {item.value})")
     def attack(self, enemy):
-            enemy.hp -= self.damage
+            enemy.health -= self.damage
             print(f"{self.name} атакує {enemy.name} на {self.damage} шкоди!")
+
+Geroi = Character("Hero",100,10)
+vrag = Enemy("Enemy",60,5)
+Treasure = Item("Сокровище",100)
+Geroi.add_item(Treasure)
+Geroi.show_inventory()
+while Geroi.health>vrag.health:
+    Geroi.attack(vrag)
+    if vrag.health<=0:
+        print(f"{vrag.name} переможений!")
+        break
+    vrag.attack(Geroi)
+    print(f"{Geroi.name} HP: {Geroi.health}")
+
+    if Geroi.health <= 0:
+        print(f"{Geroi.name} програв!")
