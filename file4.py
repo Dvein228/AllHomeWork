da12e737be89f0a2d56013001b36a280c4ccf177
Home <davidfilippov122011@gmail.com>
2026-05-30 16:02:07 +0300

    ВИКЛЮЧЕННЯ (ПОМИЛКИ). ОБРОБКА. ПОПЕРЕДЖЕННЯ

--- file4.py
+++ file4.py
@@ -1,20 +1,44 @@
 class BankAccount:
     def __init__(self, money):
         self.money = money
-    def withdraw(self, amount):
-        if amount < 0:
-            raise ValueError("Не можна знімати мінус")
-        if amount > self.money:
-            raise ValueError("Недостатньо грошей")
-        if amount==0:
-            raise ValueError("ошиибко")
-        self.money -= amount
-        print("Знято:", amount)
-        print("Залишок:", self.money)
 
-    def addMoney(self, amount):
-        if amount < 0:
-            raise ValueError("Нельзя минус закидывать")
+    def withdraw(self, amount):
         if amount == 0:
-            raise ValueError("ошиибко")
+            raise ValueError("Сума знімання не може бути 0")
+        if amount < 0:
+            raise ValueError("Сума знімання не може бути від'ємною")
+
+        if amount > self.money:
+            y_n = input("Недостатньо коштів. Оформити кредит? (y/n): ")
+            if y_n.strip().lower() == "y":
+                credit = amount - self.money
+                days = int(input("На скільки днів оформити кредит? "))
+
+                if days <= 30:
+                    rate = 0.03
+                else:
+                    rate = 0.04
+
+                repayment = credit * (1 + rate)
+
+                print(f"Кредит оформлено на: {credit} грн")
+                print(f"Відсоткова ставка: {int(rate * 100)}%")
+                print(f"Сума до повернення за {days} днів: {repayment} грн")
+
+                self.money += credit
+            else:
+                print("Операцію скасовано")
+                return
+
+        self.money -= amount
+        print(f"Знято: {amount} грн")
+        print(f"Залишок: {self.money} грн")
+
+    def add_money(self, amount):
+        if amount < 0:
+            raise ValueError("Сума поповнення не може бути від'ємною")
+        if amount == 0:
+            raise ValueError("Сума поповнення не може бути 0")
         self.money += amount
+        print(f"Рахунок поповнено на: {amount} грн")
+        print(f"Баланс: {self.money} грн")
